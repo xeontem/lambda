638c95d1e468f4817e6ea5ca89746b1f19098090
xeontem <xeontem@gmail.com>
2018-03-18 23:00:05 +0300

    konstant test

--- test/combinators.test.js
+++ test/combinators.test.js
@@ -4,5 +4,11 @@ describe('Combinators', function() {
       assert.equal(X.I('string'), 'string');
     });
   });
+
+  describe('Constant', function() {
+    it('must always return its first argument', function() {
+      assert.equal(X.K('string')(), 'string');
+    });
+  });
 });
 
